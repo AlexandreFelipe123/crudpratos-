47835757e91ddb94d8d1843b16af4adf1637dee9
AlexandreFelipe123 <128101254+AlexandreFelipe123@users.noreply.github.com>
2023-04-17 23:48:06 -0300

    Update form-add.php

--- form-add.php
+++ form-add.php
@@ -3,35 +3,51 @@ require 'init.php';
 ?>
 </doctype HTML>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Cadastro de Comidas Favoritas</title>
-		<link href = "bootstrap/css/bootstrap.css" rel="stylesheet">
-        <script src = "bootstrap/js/bootstrap.js"></script>
-	</head>
-	<body>
-		<div class="container">
-			<h1>Cadastro de comidas favoritas</h1>
-			<h2>Cadastro de Comida</h2>
+
+<head>
+	<meta charset="utf-8">
+	<title>Cadastro de Comidas Favoritas</title>
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="estilo.css">
+	<link href="https://fonts.googleapis.com/css2?family=ABeeZee&display=swap" rel="stylesheet">
+	<script src="bootstrap/js/bootstrap.js"></script>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<header class="row" id="header">
+			<a href="index.php" id="a-header">
+				<h1 id="h1-header">Cadastro de Comidas Favoritas</h1>
+			</a>
+		</header>
+	</div>
+	<div class="container my-5">
+		<div class="card p-3 mx-auto border border-danger" style="max-width: 500px;">
+			<h2 class="text-center text-danger">Cadastro de Comida </h2>
 			<form action="add.php" method="post">
-			<div class="form-group">
-				<label for ="nome">Nome: </label>
-				<input type="text" class = "form-control col-sm" name="nome" id="nome" style= "width:25%;" placeholder="Informe seu nome">
-			</div>
-			<div class="form-group">
-				<label for = "ingredientes">Ingredientes: </label>
-				<input type = "text" class = "form-control col-sm" name="ingredientes" id = "ingredientes" style="width:25%" placeholder = "Informe os ingredientes">
-			</div>
-			<div class="form-group">
-				<label for ="dificuldade_preparo">Dificuldade de preparo:  </label>
-				<input type="text" class = "form-control col-sm" name="dificuldade_preparo" id="dificuldade_preparo" style= "width:25%;" placeholder="Informe o quão difícil foi preparar esse prato">
-			</div>
-			<div class="form-group">
-				<label for = "quanto_gosta">Quanto Gosta: </label>
-				<input type = "text" class = "form-control col-sm" name="quanto_gosta" id = "quanto_gosta" style="width:25%" placeholder = "Informe o quanto você gosta desse prato">		
-			</div>
-				<button type = "submit" class = "btn btn-primary">Cadastrar</button>
+				<div class="form-group">
+					<label for="nome">Nome: </label>
+					<input type="text" class="form-control col-sm border-danger" name="nome" id="nome" 
+						placeholder="Informe o prato">
+				</div>
+				<div class="form-group">
+					<label for="ingredientes">Ingredientes: </label>
+					<input type="text" class="form-control col-sm border-danger" name="ingredientes" id="ingredientes"
+						 placeholder="Informe os ingredientes">
+				</div>
+				<div class="form-group">
+					<label for="dificuldade_preparo">Dificuldade de preparo: </label>
+					<input type="text" class="form-control col-sm border-danger" name="dificuldade_preparo" id="dificuldade_preparo"
+						 placeholder="Nível de dificuldade (0 a 10)">
+				</div>
+				<div class="form-group">
+					<label for="quanto_gosta">Quanto Gosta: </label>
+					<input type="text" class="form-control col-sm border-danger" name="quanto_gosta" id="quanto_gosta"
+						 placeholder="Nota (0 a 10)">
+				</div>
+				<button type="submit" class="btn btn-warning">Cadastrar</button>
 			</form>
-		</div>	
-		</body>
+		</div>
+	</div>
+</body>
 </html>
