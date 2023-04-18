08418483fb599a813db9748b6039186d4e42eaff
AlexandreFelipe123 <128101254+AlexandreFelipe123@users.noreply.github.com>
2023-04-17 23:46:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'init.php';
 $PDO = db_connect();
 
@@ -9,57 +9,88 @@ $stmt_count = $PDO->prepare($sql_count);
 $stmt_count->execute();
 $total = $stmt_count->fetchColumn();
 
-$stmt = $PDO -> prepare($sql);
-$stmt -> execute();
+$stmt = $PDO->prepare($sql);
+$stmt->execute();
 ?>
 <!doctype hmtl>
 <html>
-    <head>
-        <meta charset = "utf-8">
-        <title>Cadastro de Comidas Favoritas</title>
-        <link href = "bootstrap/css/bootstrap.css" rel="stylesheet">
-        <script src = "bootstrap/js/bootstrap.js"></script>
-        <style type="text/css">
-            .container{
-                margin-top: 50px;
-                margin-left: 100px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class = "container">
-            <h1>Cadastro de Comidas Favoritas</h1>
-            <p><a href = "form-add.php">Adicionar Comida</a></p>
-            <h2>Lista de Comidas</h2>
-            <p>Total de Comidas: <?php echo $total ?></p>
+
+<head>
+  <meta charset="utf-8">
+  <title>Cadastro de Comidas Favoritas</title>
+  <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+  <link rel="stylesheet" href="estilo.css">
+  <link href="https://fonts.googleapis.com/css2?family=ABeeZee&display=swap" rel="stylesheet">
+  <script src="bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+  <div class="container-fluid">
+    <header class="row" id="header">
+      <a href="index.php" id="a-header">
+        <h1 id="h1-header">Cadastro de Comidas Favoritas</h1>
+      </a>
+    </header>
+  </div>
+  <div class="container-fluid mt-5">
+    <div class="card-deck justify-content-center">
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img
+            src="https://img.freepik.com/fotos-gratis/variedade-plana-com-deliciosa-comida-brasileira_23-2148739179.jpg?w=2000"
+            class="card-img-top" alt="Cadastrar">
+          <div class="card-body">
+            <h5 class="card-title">Cadastro de Nova Comida</h5>
+            <p class="card-text">Cadastre uma nova comida para a sua lista de comidas favoritas.</p>
+            <a href="form-add.php" class="btn btn-primary">Cadastrar</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 mt-5">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Lista de Comidas Favoritas</h5>
             <?php if ($total > 0): ?>
-            <table class =  "table table striped" width = "50%" border="1">
+              <table class="table table-striped">
                 <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>ingredientes</th>
-                        <th>Dificuldade de Preparo</th>
-                        <th>Quanto gosta</th>
-                    </tr>
+                  <tr>
+                    <th>Nome</th>
+                    <th>Ingredientes</th>
+                    <th>Dificuldade de Preparo</th>
+                    <th>Quanto Gosta</th>
+                  </tr>
                 </thead>
                 <tbody>
-                    <?php while ($user = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-                    <tr>  
-                        <td><?php echo $user['nome']?></td>
-                        <td><?php echo $user['ingredientes']?></td>
-                        <td><?php echo $user['dificuldade_preparo']?></td>
-                        <td><?php echo $user['quanto_gosta']?></td>
-                        <td>
-                            <a href="form-edit.php?id=<?php echo $user['id'] ?>">Editar</a>
-                            <a href="delete.php?id=<?php echo $user['id'] ?>" onclick="return confirm('Tem certeza de que deseja remover?');">Remover</a>
-                        </td>
-                    </tr>      
-                    <?php endwhile; ?>
+                  <?php while ($pratos = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+                    <tr>
+                      <td>
+                        <?php echo $pratos['nome'] ?>
+                      </td>
+                      <td>
+                        <?php echo $pratos['ingredientes'] ?>
+                      </td>
+                      <td>
+                        <?php echo $pratos['dificuldade_preparo'] ?>
+                      </td>
+                      <td>
+                        <?php echo $pratos['quanto_gosta'] ?>
+                      </td>
+                      <td>
+                        <a href="form-edit.php?id=<?php echo $pratos['id'] ?>" class="btn btn-primary">Editar</a>
+                        <a href="delete.php?id=<?php echo $pratos['id'] ?>" class="btn btn-danger"
+                          onclick="return confirm('Tem certeza que deseja excluir?')">Excluir</a>
+                      </td>
+                    </tr>
+                  <?php endwhile; ?>
                 </tbody>
-            </table>
+              </table>
             <?php else: ?>
-            <p>Nenhum usuário registrado</p>
+              <p class="card-text">Nenhuma comida cadastrada ainda.</p>
             <?php endif; ?>
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
